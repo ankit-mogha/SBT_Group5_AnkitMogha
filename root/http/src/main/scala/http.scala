e7c1693414d3ccde13438a7f2295ea1a5f90a7fb
ankitmogha <moghaankit.am@gmail.com>
2021-01-13 18:35:49 +0530

    SBT_assignment-Group5-AnkitMogha

---
+++ root/http/src/main/scala/http.scala
@@ -0,0 +1,5 @@
+object http {
+ def main(args: Array[String]) = {
+println("Welcome to HTTP Module")
+}
+}
---
+++ root/src/main/scala/assgin.scala
@@ -0,0 +1,5 @@
+object assign {
+ def main(args: Array[String]) = {
+println("Welcome to SBT Assignment")
+}
+}
